043e7332c551f561f8bb3b9b8eebf4d937d6b6e0
Bas Bossink <bas.bossink@gmail.com>
2018-02-11 22:31:14 +0100

    Dry out using a few helpers and macro's.

--- tests/temp-converter-test.c
+++ tests/temp-converter-test.c
@@ -1,66 +1,91 @@
+#include <stdbool.h>
 #include <math.h>
+
 #include "../vendor/minunit/minunit.h"
 #include "../src/temp-converter.h"
 
+/**
+ * Definitions
+ */
 const double epsilon = 1e-3;
 
 typedef struct temperature_pair {
-  double fahrenheit;
-  double celsius;
+    double fahrenheit;
+    double celsius;
 } temperature_pair;
 
+typedef char* (*test_single_case)(
+    temperature_pair test_case,
+    unsigned long test_case_counter);
 
-#define TEST_CASE(f, c) { .fahrenheit = (f), .celsius = (c) },
+/**
+ * Helpers
+ */
+static bool approximately_equal(
+    double actual,
+    double expected) {
+    return fabs(actual - expected) < epsilon;
+}
+
+static char* run_tests(
+    temperature_pair test_cases[],
+    test_single_case test_it) {
+    char* result = NULL;
+    for (unsigned long i = 0ul;
+         !isnan(test_cases[i].fahrenheit) && !result;
+         i++) {
+        temperature_pair test = test_cases[i];
+        result = test_it(test, i);
+    }
+    return result;
+}
+
+#define single_test(fromL,toL,conversion) static char* single_from_##fromL(\
+    temperature_pair pair,\
+    unsigned long test_case_counter) {\
+    char const *message_format = "(%s:%s::%d) expected conversion of %f " #fromL " to equal %f " #toL " but got %f";\
+    double fahrenheit = pair.fahrenheit;\
+    double celsius = pair.celsius;\
+    double actual = conversion(fromL);\
+    mu_assert(\
+        approximately_equal(actual, toL),\
+        message_format,\
+        __FILE__,\
+        __func__,\
+        test_case_counter,\
+        fromL,\
+        toL,\
+        actual);\
+    return NULL;\
+}
+
+single_test(fahrenheit,celsius,convert_fahrenheit_to_celsius)
+single_test(celsius,fahrenheit,convert_celsius_to_fahrenheit)
+#undef single_test
+
+/**
+ * Test cases
+ */
+#define test_case(f, c) { .fahrenheit = (f), .celsius = (c) },
 static temperature_pair test_cases[] = {
-    TEST_CASE(32, 0)
-    TEST_CASE(33, 5.0 / 9.0)
-    TEST_CASE(41, 5.0)
-    TEST_CASE(23, -5.0)
-    TEST_CASE(50, 10.0)
-    TEST_CASE(59, 15.0)
-    TEST_CASE(95, 35.0)
-  };
-#undef TEST_CASE
+    test_case(32, 0)
+    test_case(33, 5.0 / 9.0)
+    test_case(41, 5.0)
+    test_case(23, -5.0)
+    test_case(50, 10.0)
+    test_case(59, 15.0)
+    test_case(95, 35.0)
+    test_case(NAN, NAN)
+};
+#undef test_case
 
-
-char *
-test_convert_fahrenheit_to_celsius(void) {
-  char const *message_format = "(%s:%s::%d) expected conversion of %f fahrenheit to equal %f celsius but got %f";
-  const size_t number_of_test_cases = sizeof(test_cases) / sizeof(temperature_pair);
-  for (unsigned long i = 0ul;
-       i < number_of_test_cases;
-       i++) {
-    temperature_pair test = test_cases[i];
-    double actual = convert_fahrenheit_to_celsius(test.fahrenheit);
-    mu_assert(fabs(actual - test.celsius) < epsilon,
-              message_format,
-              __FILE__,
-              __func__,
-              i,
-              test.fahrenheit,
-              test.celsius,
-              actual);
-  }
-  return 0;
+/**
+ * Tests
+ */
+#define test(name,from) char* name(void) {\
+    return run_tests(test_cases, single_from_##from);\
 }
 
-char *
-test_convert_celsius_to_fahrenheit(void) {
-  char const* message_format = "(%s:%s::%d) expected conversion of %f celsius to equal %f fahrenheit but got %f";
-  const size_t number_of_test_cases = sizeof(test_cases) / sizeof(temperature_pair);
-  for (unsigned long i = 0ul;
-       i < number_of_test_cases;
-       i++) {
-    temperature_pair test = test_cases[i];
-    double actual = convert_celsius_to_fahrenheit(test.celsius);
-    mu_assert(fabs(actual - test.fahrenheit) < epsilon,
-      message_format,
-      __FILE__,
-      __func__,
-      i,
-      test.celsius,
-      test.fahrenheit,
-      actual);
-  }
-  return 0;
-}
+test(test_convert_celsius_to_fahrenheit, celsius)
+test(test_convert_fahrenheit_to_celsius, fahrenheit)
+#undef test
